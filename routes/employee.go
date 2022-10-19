15e2688f3a3f27aec08b5b3ab4fd837aa4d90040
sopan <sopan.mukti21@gmail.com>
2022-10-19 07:33:14 +0700

    "setting db routes dan model"

--- routes/employee.go
+++ routes/employee.go
@@ -8,14 +8,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func GetEmployee(ctx *gin.Context){
+func GetEmployee(ctx *gin.Context) {
 	var employee []models.Employee
 
-err := ctx.ShouldBindJSON(&employee);err != nil{
-	ctx.JSON(http.StatusBadRequest,gin.H{
-		"status":"failed to get employee",
-		"message":"internal server error",
-	})
-}
-config.DB.Find(&employee)
+	// err := ctx.ShouldBindJSON(&employee); err != nil{
+	// 	ctx.JSON(http.StatusBadRequest,gin.H{
+	// 		"status":"failed to get employee",
+	// 		"message":"internal server error",
+	// 	}),
+	// },
+	config.DB.Find(&employee)
+
+	ctx.JSON(http.StatusAccepted, gin.H{})
 }
